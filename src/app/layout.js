6ded6333fbd3fe9b2f9770f30ee3ce01ef818ffd
Emi ni <isrealspicy@gmail.com>
2025-04-23 20:24:33 +0100

    vercel analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { CoinProvider } from '../context/coinContext'; 
 import { SpeedInsights } from '@vercel/speed-insights/next';
- 
+import { Analytics } from "@vercel/analytics/react"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,6 +28,7 @@ export default function RootLayout({ children }) {
       >
         <CoinProvider>{children}</CoinProvider>
         <SpeedInsights/>
+        <Analytics/>
       </body>
     </html>
   );
